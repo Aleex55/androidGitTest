34034262d86d8980463981f833865ccf778e7e17
JoanRoura <94047576+JoanRoura@users.noreply.github.com>
2022-10-03 11:00:25 +0200

    Comit3_Joan

--- app/src/main/java/com/example/androidgittest/MainActivity.kt
+++ app/src/main/java/com/example/androidgittest/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val hol = 4
 
         System.out.println("Hola bona tarda")
     }
